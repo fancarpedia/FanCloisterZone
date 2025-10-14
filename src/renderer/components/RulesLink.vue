39bc19995dd2bfd42701966e4da94152d4c75f6b
stanolacko <stano.lacko@gmail.com>
2025-10-14 07:59:37 +0200

    Design of Rules link

--- src/renderer/components/RulesLink.vue
+++ src/renderer/components/RulesLink.vue
@@ -4,15 +4,18 @@
     :color="color"
     label
     text-color="white"
-    :title="href"
+    :title="url"
     @click.prevent="open"
   >
     <v-icon v-if="type === 'download'" left>fa-file-download</v-icon>
-    <v-icon v-else-if="type === 'wiki' || site === 'wica'" left>fab fa-wikipedia-w</v-icon>
+    <v-icon v-else-if="type === 'wiki'" left>fab fa-wikipedia-w</v-icon>
+    <template v-else-if="site === 'wica'">
+      <img src="~/assets/wica.png" width="29" height="29">&nbsp;
+    </template>
     <template v-else-if="site === 'bgg'">
       <img src="~/assets/bgg.svg" width="60" height="29">&nbsp;
     </template>
-    {{ title }}
+    {{ $t(titlekey) }}
   </v-chip>
 </template>
 
@@ -34,14 +37,40 @@ export default {
       if (this.href.startsWith('https://boardgamegeek.com')) {
         return 'bgg'
       }
+      if (this.href.startsWith('https://www.hans')) {
+        return 'hig'
+      }
       return ''
     },
 
     color () {
       if (this.site === 'bgg') {
         return '#3f3a60'
+      } else if (this.site === 'wica') {
+        return '#2a66c2'
+      } else if (this.site === 'hig') {
+        return '#d19738'
       }
       return 'pink lighten-1'
+    },
+    
+    url () {
+      if (this.href.startsWith('https://wikicarpedia.com/index.php/') && !this.href.startsWith('https://wikicarpedia.com/index.php/Special:MyLanguage/')) {
+        return this.href.replace('https://wikicarpedia.com/index.php/','https://wikicarpedia.com/index.php/Special:MyLanguage/')
+      } else if (this.href.startsWith('https://wikicarpedia.com/car/') && !this.href.startsWith('https://wikicarpedia.com/car/Special:MyLanguage/')) {
+        return this.href.replace('https://wikicarpedia.com/car/','https://wikicarpedia.com/car/Special:MyLanguage/')
+      }
+      return this.href
+    },
+    
+    titlekey () {
+      if (this.title == 'Rules') {
+        return 'menu.rules'
+      } else if (this.site == 'hig' && this.title == '©') {
+        return 'HiG ©'
+      } else {
+        return this.title
+      }
     }
   },
 
