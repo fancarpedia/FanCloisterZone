6974aef380d9d670631ea4f0a40386a55706678b
stanolacko <stano.lacko@gmail.com>
2026-01-27 11:35:47 +0100

    Engine to 6.1.6 solving Dunkey-Tunnel interaction

--- build-scripts/download-game-engine.js
+++ build-scripts/download-game-engine.js
@@ -19,6 +19,6 @@ function download (url, w) {
 
 fs.access('Engine.jar', fs.F_OK, err => {
   if (err) {
-    download('https://github.com/fancarpedia/JCloisterZoneEngine/releases/download/v6.1.5/Engine.jar', fs.createWriteStream('Engine.jar'))
+    download('https://github.com/fancarpedia/JCloisterZoneEngine/releases/download/v6.1.6/Engine.jar', fs.createWriteStream('Engine.jar'))
   }
 })
