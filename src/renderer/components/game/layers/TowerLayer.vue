ba944340d22c3641c9cffacc0ca5b51f3187dccb
stanolacko <stano.lacko@gmail.com>
2026-02-13 15:07:58 +0100

    Fixed Tower shapes (missing scale for shapes by tile dimensions)

--- src/renderer/components/game/layers/TowerLayer.vue
+++ src/renderer/components/game/layers/TowerLayer.vue
@@ -1,13 +1,13 @@
 <template>
   <g id="tower-layer">
     <g
-      v-for="({ height, tile, point, rotation, fill, textColor, stroke }) in towerItems"
+      v-for="({ height, tile, point, rotation, transform, fill, textColor, stroke }) in towerItems"
       :key="positionAsKey(tile.position)"
-      :transform="`${transformPosition(tile.position)} ${transformRotation(rotation)} translate(${point[0]} ${point[1]}) rotate(${-rotation} 0 0)`"
+      :transform="`${transformPosition(tile.position)} ${transformRotation(rotation)} ${transform} translate(${point[0]} ${point[1]}) rotate(${-rotation} 0 0)`"
     >
       <rect
         class="tower"
-        x="-170" y="-140" width="340" height="280"
+        x="-170" y="-170" width="340" height="340"
         :stroke="stroke" stroke-width="40" stroke-opacity="0.8" stroke-dasharray="38"
         :fill="fill" fill-opacity="0.65"
       />
@@ -31,7 +31,7 @@ export default {
 
     towerItems () {
       return this.towers.map(t => {
-        const { tile, point, rotation } = this.getTilePoint({ position: t.places[0], feature: 'Tower', location: 'I' })
+        const { tile, point, rotation, transform } = this.getTilePoint({ position: t.places[0], feature: 'Tower', location: 'I' })
         let fill = 'burlywood'
         let color = 'black'
         let stroke = 'black'
@@ -43,11 +43,13 @@ export default {
           color = 'white'
           stroke = 'white'
         }
+        console.log(transform)
         return {
           height: t.height,
           tile,
           point,
           rotation,
+          transform,
           fill,
           textColor: color,
           stroke
