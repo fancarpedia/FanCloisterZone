8cc657c7800ef7ef8a582ab704eb3b916aa8f540
stanolacko <stano.lacko@gmail.com>
2026-02-13 14:59:00 +0100

    Tower stroke reflects by tower piece

--- src/renderer/components/game/layers/TowerLayer.vue
+++ src/renderer/components/game/layers/TowerLayer.vue
@@ -1,14 +1,14 @@
 <template>
   <g id="tower-layer">
     <g
-      v-for="({ height, tile, point, rotation, fill, textColor }) in towerItems"
+      v-for="({ height, tile, point, rotation, fill, textColor, stroke }) in towerItems"
       :key="positionAsKey(tile.position)"
       :transform="`${transformPosition(tile.position)} ${transformRotation(rotation)} translate(${point[0]} ${point[1]}) rotate(${-rotation} 0 0)`"
     >
       <rect
         class="tower"
         x="-170" y="-140" width="340" height="280"
-        stroke="black" stroke-width="40" stroke-opacity="0.8" stroke-dasharray="38"
+        :stroke="stroke" stroke-width="40" stroke-opacity="0.8" stroke-dasharray="38"
         :fill="fill" fill-opacity="0.65"
       />
       <text x="-60" y="65" :fill="textColor" style="font-size: 192px; font-weight: 600">{{ height }}</text>
@@ -34,12 +34,14 @@ export default {
         const { tile, point, rotation } = this.getTilePoint({ position: t.places[0], feature: 'Tower', location: 'I' })
         let fill = 'burlywood'
         let color = 'black'
+        let stroke = 'black'
         if (t.lastPiece == 'WHITE_TOWER_PIECE') {
           fill = 'white'
           color = 'transparent'
         } else if (t.lastPiece == 'BLACK_TOWER_PIECE') {
           fill = 'black'
           color = 'white'
+          stroke = 'white'
         }
         return {
           height: t.height,
@@ -47,7 +49,8 @@ export default {
           point,
           rotation,
           fill,
-          textColor: color
+          textColor: color,
+          stroke
         }
       })
     }
