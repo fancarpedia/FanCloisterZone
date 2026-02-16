f042db5ee15694de7a8a2b1ac58af48d0a4a917b
stanolacko <stano.lacko@gmail.com>
2026-02-16 15:18:59 +0100

    Engine v6.1.18

--- build-scripts/download-game-engine.js
+++ build-scripts/download-game-engine.js
@@ -19,6 +19,6 @@ function download (url, w) {
 
 fs.access('Engine.jar', fs.F_OK, err => {
   if (err) {
-    download('https://github.com/fancarpedia/JCloisterZoneEngine/releases/download/v6.1.17/Engine.jar', fs.createWriteStream('Engine.jar'))
+    download('https://github.com/fancarpedia/JCloisterZoneEngine/releases/download/v6.1.18/Engine.jar', fs.createWriteStream('Engine.jar'))
   }
 })
