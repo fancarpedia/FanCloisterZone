502308912f09919ff504c19cae5b1e805c50ffda
stanolacko <stano.lacko@gmail.com>
2026-02-17 16:42:22 +0100

    Bump to Engine 6.1.20

--- build-scripts/download-game-engine.js
+++ build-scripts/download-game-engine.js
@@ -19,6 +19,6 @@ function download (url, w) {
 
 fs.access('Engine.jar', fs.F_OK, err => {
   if (err) {
-    download('https://github.com/fancarpedia/JCloisterZoneEngine/releases/download/v6.1.19/Engine.jar', fs.createWriteStream('Engine.jar'))
+    download('https://github.com/fancarpedia/JCloisterZoneEngine/releases/download/v6.1.20/Engine.jar', fs.createWriteStream('Engine.jar'))
   }
 })
