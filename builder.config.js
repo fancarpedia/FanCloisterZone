420c840506c730e8d5700c952e39e044309da99c
stanolacko <stano.lacko@gmail.com>
2026-02-21 11:38:30 +0100

    MacOS

--- builder.config.js
+++ builder.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const fs = require('fs')
 
+const ICONS_DIR = 'build/icons/'
+
 async function afterPack(context) {
   if (context.electronPlatformName !== 'darwin') return
 
@@ -48,4 +50,67 @@ function removeByExtension(baseDir, ext) {
     }
   }
   walk(baseDir)
+}
+
+const windowsOS = {
+  win: {
+    icon: ICONS_DIR + 'win-icon-fan-6.1.ico',
+    publisherName: 'farin',
+    target: 'nsis'
+  },
+  nsis: {
+    differentialPackage: true
+  }
+}
+
+const linuxOS = {
+  linux: {
+    icon: ICONS_DIR,
+    target: 'AppImage'
+  }
+}
+
+const macOS = {
+  mac: {
+    target: [
+      {
+        target: 'dmg',
+        arch: ['universal']
+      }
+    ],
+    icon: ICONS_DIR + 'fcz-icon.icns',
+    minimumSystemVersion: '10.13.6',
+    identity: null  // disable code signing on CI
+  },
+  dmg: {
+    contents: [
+      { x: 410, y: 150, type: 'link', path: '/Applications' },
+      { x: 130, y: 150, type: 'file' }
+    ]
+  }
+}
+
+module.exports = {
+  asar: false,
+  productName: 'FanCloisterZone',
+  appId: 'com.jcloisterzone.fan',
+  artifactName: 'fancloisterzone-${version}.${ext}',
+  directories: {
+    output: 'build'
+  },
+  files: [
+    'package.json',
+    { from: 'dist/main/', to: 'dist/main/' },
+    { from: 'dist/renderer', to: 'dist/renderer/' },
+    { from: 'src/resources/', to: 'dist/resources/' }
+  ],
+  extraResources: [
+    'Engine.jar',
+    'icons',
+    { from: 'src/extraResources/', to: '' }
+  ],
+  afterPack,
+  ...windowsOS,
+  ...linuxOS,
+  ...macOS
 }
