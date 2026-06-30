c5e21f503b05d94c64ad950559cbe052e8a88ab3
stanolacko <stano.lacko@gmail.com>
2026-06-30 21:03:18 +0200

    Test results hide/show (DEV stuff)

--- src/renderer/components/game/TestResult.vue
+++ src/renderer/components/game/TestResult.vue
@@ -1,23 +1,24 @@
 <template>
   <v-card class="test-result">
     <div class="btn-line">
-      <v-btn @click="$router.push('/test-runner')">{{ $nuxt.$t('button.test-runner') }}</v-btn>
+      <v-btn @click="casesHidden = !casesHidden">{{ casesHidden ? $nuxt.$t('button.show') : $nuxt.$t('button.hide') }}</v-btn>
       <v-btn @click="$router.push('/')">{{ $nuxt.$t('button.close') }}</v-btn>
-      <v-btn @click="$store.commit('game/testScenarioResult', null)">{{ $nuxt.$t('button.hide') }}</v-btn>
     </div>
     <div class="description">
       <b>{{ $nuxt.$t('dev.test') }}:</b><br>
       {{ result.description }}
     </div>
-    <div
-      v-for="(ar, idx) in result.assertions"
-      :key="idx"
-      class="d-flex"
-      :class="{ case: true, ok: ar.result, fail: !ar.result  }"
-    >
-      <span class="flex-grow-1 mr-5">{{ ar.assertion }}</span>
-      <span class="pr-1">{{ ar.result ? $nuxt.$t('dev.ok') : ar.error || $nuxt.$t('dev.fail') }}</span>
-    </div>
+    <template v-if="!casesHidden">
+      <div
+        v-for="(ar, idx) in result.assertions"
+        :key="idx"
+        class="d-flex"
+        :class="{ case: true, ok: ar.result, fail: !ar.result  }"
+      >
+        <span class="flex-grow-1 mr-5">{{ ar.assertion }}</span>
+        <span class="pr-1">{{ ar.result ? $nuxt.$t('dev.ok') : ar.error || $nuxt.$t('dev.fail') }}</span>
+      </div>
+    </template>
   </v-card>
 </template>
 
@@ -25,6 +26,12 @@
 export default {
   props: {
     result: { type: Object, required: true }
+  },
+
+  data () {
+    return {
+      casesHidden: false
+    }
   }
 }
 </script>
